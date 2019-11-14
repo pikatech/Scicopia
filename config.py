fa1f3ef014645d8f8ad443c760c3933af9ffca9c
Tech <pikatech@mail.de>
2019-11-14 17:56:10 +0100

    Add config module

---
+++ config.py
@@ -0,0 +1,27 @@
+from collections import namedtuple
+import json
+from os.path import exists
+import sys
+
+Config = namedtuple('Config', ['username', 'password', 'secret', 'hosts',
+                               'index', 'database', 'collection'])
+CONFIG = 'config.json'
+
+def read_config():
+    if exists(CONFIG):
+        with open(CONFIG) as config:
+            conf = json.load(config)
+    else:
+        print("Could not find configuration file {0}.".format(CONFIG))
+        sys.exit(1)
+    
+    return Config(
+        username = conf['username'],
+        password = conf['password'],
+        secret = conf['secret_key'],
+        hosts = conf['es_hosts'],
+        index = conf['index'],
+        database = conf['database'],
+        collection = conf['collection']
+    )
+
