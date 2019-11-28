0a2ef9ff3dced9a5699bde485798fb8f8a01ead0
RonjaKarmann <ronja.maria.karmann@uni-jena.de>
2019-11-28 17:34:10 +0100

    create collection for user in arangodb

---
+++ initusercollection.py
@@ -0,0 +1,16 @@
+from pyArango.connection import Connection
+from pyArango.theExceptions import CreationError
+
+from config import read_config
+    
+config = read_config()
+
+arangoconn = Connection(username = config.username, password = config.password) # connection to ArangoDB
+if arangoconn.hasDatabase(config.database):
+    db = arangoconn[config.database]
+else:
+    db = arangoconn.createDatabase(name = config.database) 
+if db.hasCollection(config.usercollection):
+    coll = db[config.usercollection]
+else:
+    coll = db.createCollection(name = config.usercollection)
