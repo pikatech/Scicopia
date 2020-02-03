9ea2011668841e9515c05dcdc6e09a62a0a63e33
RonjaKarmann <ronja.maria.karmann@uni-jena.de>
2020-02-03 12:26:47 +0100

    parser for bibtex

---
+++ parser/bib_parse.py
@@ -0,0 +1,42 @@
+import logging
+from typing import Dict
+
+from pybtex.database import parse_file
+from pybtex.exceptions import PybtexError
+
+allowed = {'author', 'editor', 'publisher', 'institution', 'title',
+        'booktitle', 'abstract', 'keywords', 'year', 'pages', 'journal',
+        'volume', 'number', 'doi', 'cited-by', 'citing'}
+
+
+
+def bib(bibfile) -> Dict[str, str]:
+    bib_format = 'bibtex'
+    try:
+        bib_data = parse_file(bibfile, bib_format)
+        for entry in bib_data.entries.itervalues():
+            datadict = dict()
+            datadict['id'] = entry.key
+            datadict['entrytype'] = entry.type
+            for field in entry.fields.items():
+                fieldname = field[0].lower()
+                if fieldname in allowed:
+                    # Non-standard field in personal library
+                    if field[0] == 'cited-by':
+                        datadict['cited_by'] = field[1]
+                    else:
+                        datadict[fieldname] = field[1]
+            for item in entry.persons.items():
+                persons = []
+                for person in item[1]:
+                    names = []
+                    names.extend(person.first_names)
+                    names.extend(person.middle_names)
+                    names.extend(person.prelast_names)
+                    names.extend(person.lineage_names)
+                    names.extend(person.last_names)
+                    persons.append(' '.join(names))
+                datadict[item[0]] = persons
+            yield datadict
+    except PybtexError as p:
+        logging.error(f'{p}\n')
