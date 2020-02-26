7cc0a00a06e50ebdc18e4b769688cfa9f2963bf4
Tech <pikatech@mail.de>
2020-02-26 11:14:55 +0100

    Perform key validation for ArangoDB
    Change characters that are not in the valid set mentioned on
    https://www.arangodb.com/docs/stable/
    data-modeling-naming-conventions-document-keys.html
    into an underscore.
    
    The code also got formatted by Black.

--- scicopia/arangodoc.py
+++ scicopia/arangodoc.py
@@ -9,9 +9,10 @@ Created on Tue Aug 27 16:18:45 2019
 import glob
 from io import BufferedReader, TextIOWrapper
 import logging
-import os
 import argparse
 import base64
+import os
+import re
 from contextlib import contextmanager
 from collections import defaultdict
 from typing import Dict
@@ -30,10 +31,15 @@ from pyArango.connection import Connection
 from pyArango.theExceptions import DocumentNotFoundError, CreationError
 from config import read_config
 
+# See: https://www.arangodb.com/docs/stable/data-modeling-naming-conventions-document-keys.html
+not_valid = re.compile("[^-_:.@()+,=;$!*'%A-Za-z0-9]")
+
 
 def create_id(doc: Dict, doc_format: str) -> None:
-    if doc_format == 'pubmed':
-        doc['id'] = f'PMID{doc["PMID"]}'
+    if doc_format == "pubmed":
+        doc["id"] = f'PMID{doc["PMID"]}'
+
+
 #   elif doc_format == 'arxiv':
 #       pass
 #   elif format == 'bibtex':
@@ -41,8 +47,10 @@ def create_id(doc: Dict, doc_format: str) -> None:
 
 
 @contextmanager
-def zstd_open(filename: str, mode: str = 'rb', encoding: str = 'utf-8') -> BufferedReader:
-    '''
+def zstd_open(
+    filename: str, mode: str = "rb", encoding: str = "utf-8"
+) -> BufferedReader:
+    """
     This is an auxilliary function to provide an open() function which supports
     readline(), as ZstdDecompressor and the object produced by
     ZstdDecompressor.stream_reader() don't have one.'
@@ -63,100 +71,127 @@ def zstd_open(filename: str, mode: str = 'rb', encoding: str = 'utf-8') -> Buffe
     BufferedReader
         DESCRIPTION.
 
-    '''
+    """
     dctx = zstd.ZstdDecompressor()
-    with open(filename, mode='rb') as fh:
+    with open(filename, mode="rb") as fh:
         with dctx.stream_reader(fh) as reader:
             yield TextIOWrapper(BufferedReader(reader, 32768), encoding=encoding)
 
 
 def setup():
     config = read_config()
-    if 'arango_url' in config:
-        arangoconn = Connection(arangoURL = config['arango_url'],
-                                username = config['username'],
-                                password = config['password'])
+    if "arango_url" in config:
+        arangoconn = Connection(
+            arangoURL=config["arango_url"],
+            username=config["username"],
+            password=config["password"],
+        )
     else:
-        arangoconn = Connection(username = config['username'],
-                                password = config['password'])
-        
-    if arangoconn.hasDatabase(config['database']):
-        db = arangoconn[config['database']]
+        arangoconn = Connection(
+            username=config["username"], password=config["password"]
+        )
+
+    if arangoconn.hasDatabase(config["database"]):
+        db = arangoconn[config["database"]]
     else:
-        db = arangoconn.createDatabase(name = config['database']) 
-    if db.hasCollection(config['collection']):
-        collection = db[config['collection']]
+        db = arangoconn.createDatabase(name=config["database"])
+    if db.hasCollection(config["collection"]):
+        collection = db[config["collection"]]
     else:
-        collection = db.createCollection(name = config['collection'])
+        collection = db.createCollection(name=config["collection"])
     return collection
 
+
 def pdfsave(file):
-    file = f'{file[:file.rindex(".")]}.pdf' # muss ich noch verbessern
+    file = f'{file[:file.rindex(".")]}.pdf'  # muss ich noch verbessern
     try:
-        with open(file, 'rb') as f:
+        with open(file, "rb") as f:
             data = base64.b64encode(f.read())
             data = data.decode()
     except FileNotFoundError:
-        data = ''
+        data = ""
     return data
 
-def main(doc_format, path = '', pdf = False, recursive = False, compression = None, update = False):
+
+def main(
+    doc_format, path="", pdf=False, recursive=False, compression=None, update=False
+):
     collection = setup()
     path = path if path.endswith(os.path.sep) else path + os.path.sep
-    typedict = defaultdict(lambda:'.xml')
-    typedict['bib'] = '.bib'
-    zipdict = defaultdict(lambda:'')
-    zipdict.update({'gzip':'.gz', 'bzip2':'.bz2','zstd':'.zstd','zip':'.zip'})
-    fundict = {'bib': bib, 'pubmed': pubmed, 'arxiv': arxiv, 'grobid': grobid}
-    opendict = defaultdict(lambda:open)
-    opendict.update({'gzip':gzip.open, 'bzip':bz2.open, 'zstd':zstd_open})
-    f = f'**{os.path.sep}' if recursive else ''
-    files = glob.glob(f'{path}{f}*{typedict[doc_format]}{zipdict[compression]}', recursive = recursive)
-    logging.info(f'{len(files)} {typedict[doc_format]}{zipdict[compression]}-files found')
-    bar = Bar('files', max=len(files))
+    typedict = defaultdict(lambda: ".xml")
+    typedict["bib"] = ".bib"
+    zipdict = defaultdict(lambda: "")
+    zipdict.update({"gzip": ".gz", "bzip2": ".bz2", "zstd": ".zstd", "zip": ".zip"})
+    fundict = {"bib": bib, "pubmed": pubmed, "arxiv": arxiv, "grobid": grobid}
+    opendict = defaultdict(lambda: open)
+    opendict.update({"gzip": gzip.open, "bzip": bz2.open, "zstd": zstd_open})
+    f = f"**{os.path.sep}" if recursive else ""
+    files = glob.glob(
+        f"{path}{f}*{typedict[doc_format]}{zipdict[compression]}", recursive=recursive
+    )
+    logging.info(
+        f"{len(files)} {typedict[doc_format]}{zipdict[compression]}-files found"
+    )
+    bar = Bar("files", max=len(files))
     for file in files:
         first = True
-        with opendict[compression](file, 'rt', encoding='utf-8') as data:
+        with opendict[compression](file, "rt", encoding="utf-8") as data:
+            docs = [None] * 100
             for entry in fundict[doc_format](data):
                 create_id(entry, doc_format)
                 if update:
                     try:
-                        doc = collection[entry['id']]
+                        doc = collection[entry["id"]]
                     except DocumentNotFoundError:
                         doc = collection.createDocument()
-                        doc._key = entry['id']
+                        doc._key = entry["id"]
                 else:
                     doc = collection.createDocument()
-                    doc._key = entry['id']
-                    print(entry['id'])
+                    doc_id = entry["id"]
+                    # Make sure document keys are valid
+                    doc._key = re.sub(not_valid, "_", doc_id)
                 for field in entry:
-                    if field == 'id':
+                    if field == "id":
                         continue
                     doc[field] = entry[field]
                 if pdf:
                     data = pdfsave(file)
                     if data:
-                        doc['pdf'] = data
+                        doc["pdf"] = data
                     else:
                         if first:
-                            logging.warning(f'No PDF found for {file}\n')
+                            logging.warning(f"No PDF found for {file}\n")
                             first = False
                 try:
                     doc.save()
                 except CreationError:
-                    logging.warning(f'Key {entry["id"]} already exists. To update the Data use --update\n')
+                    logging.warning(
+                        f'Key {entry["id"]} already exists. To update the Data use --update\n'
+                    )
         bar.next()
     bar.finish()
 
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Saves the Data in an Arangodatabase')
-    parser.add_argument('type', choices=['bib', 'pubmed', 'arxiv', 'grobid'], help='Type of the Data')
-    parser.add_argument('--path', help='Path to the directory', default='')
-    parser.add_argument('--pdf', help='Search and Save PDFs of the bib Data', action='store_true')
-    parser.add_argument('-r', '--recursive', help='Searching of Subdirectory', action='store_true')
-    parser.add_argument('-c', '--compression', choices=['zip', 'gzip', 'zstd', 'bzip2'], help='Archive?', default=None)
-    parser.add_argument('--update', help='update arango if true', action='store_true')
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Saves the Data in an Arangodatabase")
+    parser.add_argument(
+        "type", choices=["bib", "pubmed", "arxiv", "grobid"], help="Type of the Data"
+    )
+    parser.add_argument("--path", help="Path to the directory", default="")
+    parser.add_argument(
+        "--pdf", help="Search and Save PDFs of the bib Data", action="store_true"
+    )
+    parser.add_argument(
+        "-r", "--recursive", help="Searching of Subdirectory", action="store_true"
+    )
+    parser.add_argument(
+        "-c",
+        "--compression",
+        choices=["zip", "gzip", "zstd", "bzip2"],
+        help="Archive?",
+        default=None,
+    )
+    parser.add_argument("--update", help="update arango if true", action="store_true")
 
     args = parser.parse_args()
     main(args.type, args.path, args.pdf, args.recursive, args.compression, args.update)
-
