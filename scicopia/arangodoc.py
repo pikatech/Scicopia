e9c9e90acf57d14899c151bb4088aebf4c0924f2
Tech <pikatech@mail.de>
2020-02-26 15:06:57 +0100

    Add batch updates via bulkSave

--- scicopia/arangodoc.py
+++ scicopia/arangodoc.py
@@ -14,7 +14,7 @@ import base64
 import os
 import re
 from contextlib import contextmanager
-from collections import defaultdict
+from collections import defaultdict, deque
 from typing import Dict
 from progress.bar import Bar
 
@@ -28,7 +28,7 @@ from parser.arxiv import parse as arxiv
 from parser.grobid import parse as grobid
 
 from pyArango.connection import Connection
-from pyArango.theExceptions import DocumentNotFoundError, CreationError
+from pyArango.theExceptions import DocumentNotFoundError, CreationError, UpdateError
 from config import read_config
 
 # See: https://www.arangodb.com/docs/stable/data-modeling-naming-conventions-document-keys.html
@@ -114,7 +114,8 @@ def pdfsave(file):
 
 
 def main(
-    doc_format, path="", pdf=False, recursive=False, compression=None, update=False
+    doc_format, path="", pdf=False, recursive=False, compression=None, update=False,
+    batch_size: int=100
 ):
     collection = setup()
     path = path if path.endswith(os.path.sep) else path + os.path.sep
@@ -136,7 +137,7 @@ def main(
     for file in files:
         first = True
         with opendict[compression](file, "rt", encoding="utf-8") as data:
-            docs = [None] * 100
+            docs = deque(maxlen=batch_size)
             for entry in fundict[doc_format](data):
                 create_id(entry, doc_format)
                 if update:
@@ -162,12 +163,23 @@ def main(
                         if first:
                             logging.warning(f"No PDF found for {file}\n")
                             first = False
+                docs.append(doc)
+                if len(docs) == docs.maxlen:
+                    try:
+                        collection.bulkSave(docs, details=True)
+                    except UpdateError as e:
+                        logging.error(e.message)
+                        logging.error(e.errors)
+                    finally:
+                        docs.clear()
+            if len(docs) != 0:
                 try:
-                    doc.save()
-                except CreationError:
-                    logging.warning(
-                        f'Key {entry["id"]} already exists. To update the Data use --update\n'
-                    )
+                    collection.bulkSave(docs, details=True)
+                except UpdateError as e:
+                    logging.error(e.message)
+                    logging.error(e.errors)
+                finally:
+                    docs.clear()
         bar.next()
     bar.finish()
 
