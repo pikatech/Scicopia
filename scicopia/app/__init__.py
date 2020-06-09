c86d08a9542ccb4e4ef1902d54dbc94dd9238d99
Tech <pikatech@mail.de>
2020-06-09 14:24:00 +0200

    Fix relative import

--- scicopia/app/__init__.py
+++ scicopia/app/__init__.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask_bootstrap import Bootstrap
 from flask_mail import Mail
-from ..flask_config import config
+from scicopia.flask_config import config
 
 bootstrap = Bootstrap()
 mail = Mail()
