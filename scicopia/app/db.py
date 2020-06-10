60a2e95ccaf64143383ccab8c418555835e9d760
Tech <pikatech@mail.de>
2020-06-10 16:34:47 +0200

    Let Elasticsearch do the highlighting

--- scicopia/app/db.py
+++ scicopia/app/db.py
@@ -11,42 +11,49 @@ def execute_query():
         conditions.append(Q(condition))
     prepared_search = current_app.config["SEARCH"].sort({"year": {"order": "desc"}})
     prepared_search = prepared_search.query(Q({"bool": {"must": conditions}}))
+    prepared_search = prepared_search.highlight('abstract')
+    prepared_search = prepared_search.highlight_options(pre_tags=["<b>"],
+                                                        post_tags=["</b>"],
+                                                        number_of_fragments=0)
     hits = []
     tags = []
     from_hit = session["from_hit"]
     to_hit = session["to_hit"]
     prepared_search.aggs.bucket("by_tag", "terms", field="auto_tags")
-    results = prepared_search[from_hit:to_hit].execute()
-    if results.hits.total.value != 0:
-        for r in results:
+    response = prepared_search[from_hit:to_hit].execute()
+    if response.hits.total.value != 0:
+        for r in response:
             hit = {"id": r.meta.id}
             if "title" in r:
                 hit["title"] = r.title
             if "author" in r:
                 hit["author"] = " and ".join(r.author)
-            if "abstract" in r:
-                abstractparts = []
-                for abstract in r.abstract:
-                    data = session["query"]
-                    dataparts = current_app.config["NLP"](data)
-                    for data in dataparts:
-                        data = data.text
-                        if data.lower() in abstract.lower() and len(data) > 1:
-                            abstract = abstract.lower()
-                            abstract = abstract.replace(
-                                data.lower(), f"<b>{data.lower()}</b>"
-                            )
-                            match = current_app.config["URL_MATCHER"].search(abstract)
-                            if match is None:
-                                abstractparts.append(abstract)
-                            # This will only match the first hit
-                            else:
-                                abstractparts.append(
-                                    f'{abstract[: match.start()]}<a href="{match.group(0)}">{match.group(0)}</a>{abstract[match.end(): ]}'
-                                )
-                hit["abstract"] = abstractparts
+            if "highlight" in r.meta and "abstract" in r.meta.highlight:
+                hit["abstract"] = r.meta.highlight.abstract                
+            elif "abstract" in r:
+#                 abstractparts = []
+#                 for abstract in r.abstract:
+#                     data = session["query"]
+#                     dataparts = current_app.config["NLP"](data)
+#                     for data in dataparts:
+#                         data = data.text
+#                         if data.lower() in abstract.lower() and len(data) > 1:
+# #                            abstract = abstract.lower()
+# #                            abstract = abstract.replace(
+# #                                data.lower(), f"<b>{data.lower()}</b>"
+# #                            )
+#                             match = current_app.config["URL_MATCHER"].search(abstract)
+#                             if match is None:
+#                                 abstractparts.append(abstract)
+#                             # This will only match the first hit
+#                             else:
+#                                 abstractparts.append(
+#                                     f'{abstract[: match.start()]}<a href="{match.group(0)}">{match.group(0)}</a>{abstract[match.end(): ]}'
+#                                 )
+#                hit["abstract"] = abstractparts
+                hit["abstract"] = r.abstract
             hits.append(hit)
-        for item in results.aggregations.by_tag.buckets:
+        for item in response.aggregations.by_tag.buckets:
             tag = {"name": item.key, "nr": item.doc_count, "mark": False}
             if next(
                 (
@@ -61,8 +68,8 @@ def execute_query():
 
     g.hits = hits
     session["tags"] = tags
-    session["time"] = str(results.took)
-    session["total_hits"] = results.hits.total.value
+    session["time"] = str(response.took)
+    session["total_hits"] = response.hits.total.value
 
 
 def add_search(data):
