663cd651d71f44fdbf97e6bf585a372ba8d88a8b
Tech <pikatech@mail.de>
2020-06-18 21:17:41 +0200

    Run app conditionally

--- scicopia/flask_main.py
+++ scicopia/flask_main.py
@@ -1,3 +1,5 @@
 import os
 from .app import create_app
-app = create_app(os.getenv('FLASK_CONFIG') or 'default')
+
+if __name__ == "scicopia.flask_main":
+	app = create_app(os.getenv('FLASK_CONFIG') or 'default')
