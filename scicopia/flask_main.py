3aaacab8f4e91a34f45361a9dd9f9e8131f525f4
Tech <pikatech@mail.de>
2020-07-17 15:46:22 +0200

    Create WSGI entrypoint

--- scicopia/flask_main.py
+++ scicopia/flask_main.py
@@ -1,5 +1,10 @@
 import os
+from flask import Flask
 from .app import create_app
+
+def wsgi():
+    app = create_app(os.getenv('FLASK_CONFIG') or 'default')
+    return app
 
 if __name__ == "scicopia.flask_main":
 	app = create_app(os.getenv('FLASK_CONFIG') or 'default')
