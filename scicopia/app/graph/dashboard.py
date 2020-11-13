981f6ed97f297e8e46f045841475398752eb688b
RonjaKarmann <ronja.maria.karmann@uni-jena.de>
2020-11-13 10:44:32 +0100

    add search

--- scicopia/app/graph/dashboard.py
+++ scicopia/app/graph/dashboard.py
@@ -1,81 +1,150 @@
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
+from dash.dependencies import Input, Output, State
+from dash.exceptions import PreventUpdate
 import networkx as nx
 import plotly.graph_objs as go
 from flask import current_app, render_template
-from bs4 import BeautifulSoup
+from collections import defaultdict
 
 
 def create_dashboard(server):
     dash_app = dash.Dash(
-        server=server
+        server=server,
+        external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
     )
 
     nodes = []
     for collection in current_app.config["NODECOLLECTIONS"]:
-        AQL = f"FOR x in {collection} RETURN [x._id, x]"
+        AQL = f"FOR x in {collection} RETURN [x.type, [x._id, x]]"
         nodes += current_app.config["DB"].AQLQuery(AQL, rawResults=True, batchSize=1000, ttl=3600)
+        nodedict = defaultdict(list)
+        for entry in nodes:
+            nodedict[entry[0]].append(entry[1])
 
     edges = []
     for collection in current_app.config["EDGECOLLECTIONS"]:
         AQL = f"FOR x in {collection} RETURN [x._from, x._to, x]"
         edges += current_app.config["DB"].AQLQuery(AQL, rawResults=True, batchSize=1000, ttl=3600)
 
-    legende = {'None': 'None'}
+    legende = {}
                          
     # Create Dash Layout
     dash_app.layout = html.Div(
+        className = "row",
         children=[
             html.Div(
+                className = "ten columns",
                 style={"maxWidth": "50vw"},
-                children=[dcc.Graph(id="my-graph", figure=network_graph(nodes, edges, legende))],
+                children=[dcc.Graph(id="my-graph", figure=network_graph(nodedict, edges, legende))],
             ),
+            get_checklist(legende),
+            get_dropdown(legende),
             html.Div(
-                style={"maxHeight": "400px", "maxWidth": "200px", "overflow": "auto"},
+                className="two columns",
                 children=[
-                    dcc.RadioItems(
-                        id='namelist',
-                        options=[{'label': i, 'value': i} for i in legende],
-                        value = 'None'
-                    )
-                ]
-            )
+                    dcc.Input(id="input", type="text", placeholder="search"),
+                    html.Div(id="output"),
+                    dcc.Dropdown(
+                        id="drop",
+                        options=[
+                            {'label': 'Type', 'value': 'type'},
+                            {'label': 'Name', 'value': 'name'},
+                            {'label': 'ID', 'value': '_id'}
+                        ],
+                        value=[],
+                        multi=True
+                    ),
+                    dcc.Checklist(
+                        id='check',
+                        options=[{'label':x,'value': x} for x in nodedict
+                        ],
+                        value=[]
+                    ),
+                ],
+            ),
+            
+            html.Div([], id='previously-selected', style={'display': 'none'}),
+            html.Div([], id='previously-selected2', style={'display': 'none'}),
+            html.Div([], id='previously-selected3', style={'display': 'none'}),
         ]
     )
     
-    init_callbacks(dash_app, nodes, edges, legende)
+    init_callbacks(dash_app, nodedict, edges, legende)
     
     @server.route("/dash")
     def graph():
-        soup = BeautifulSoup(dash_app.index(), 'html.parser')
-        footer = soup.footer
-        return render_template('layout.html', footer=footer)
+        return render_template('layout.html', footer=dash_app.index())
 
     return dash_app.server
 
-def init_callbacks(app, nodes, edges, legende):
+def init_callbacks(app, nodedict, edges, legende):
     @app.callback(
-        dash.dependencies.Output('my-graph', 'figure'),
-        [dash.dependencies.Input('namelist', 'value')]
+        [Output('my-graph', 'figure'), Output('dropdown-container', 'children'), Output('previously-selected', 'children'), Output('previously-selected3', 'children')],
+        [Input('input', 'value'), Input('drop', 'value'),Input('check', 'value'), Input('namelist', 'value')],
+        [State('previously-selected', 'children'), State('previously-selected2', 'children'), State('previously-selected3', 'children')]
     )
-    def update_graph(value):
-        if value != 'None':
-            sonder = {"pos": legende[value]["pos"]}
-        else:
-            sonder = None
-        return network_graph(nodes, edges, legende, sonder)
+    def update_graph(input, drop, check, value, prev_selected, prev_selected2, prev_selected3):
+        if sorted(value) == sorted(prev_selected) and sorted(prev_selected) != sorted(prev_selected2) and (input, drop, check) == prev_selected3:
+            raise PreventUpdate#return network_graph(nodedict, edges, legende, search = input, drop = drop, check = check, sonder = prev_selected2), get_dropdown(legende, value=prev_selected2), prev_selected2
+        return network_graph(nodedict, edges, legende, search = input, drop = drop, check = check, sonder = value), get_dropdown(legende, value=value), value, (input, drop, check)
         
     @app.callback(
-        dash.dependencies.Output('namelist', 'value'),
-        [dash.dependencies.Input('my-graph', 'clickData')])
-    def display_click_data(clickData):
-        if clickData:
+        Output('namelist', 'value'),
+        [Input('namedrop', 'value')],
+        [State('previously-selected', 'children'), State('previously-selected2', 'children')]
+    )
+    def display_namelist(value, prev_selected, prev_selected2):
+        if sorted(value) == sorted(prev_selected) and sorted(prev_selected) != sorted(prev_selected2):
+            raise PreventUpdate
+        return value
+
+    @app.callback(
+        [Output('checklist-container', 'children'), Output('previously-selected2', 'children')],
+        [Input('my-graph', 'clickData')],
+        [State('previously-selected', 'children')]
+    )
+    def display_click_data(clickData, prev_selected):
+        if clickData and 'text' in clickData['points'][0]:
             text = clickData['points'][0]['text']
             text = text[4:text.index("<br>")]
             if text in legende:
-                return text
-        return "None"
+                id = legende[text]['id']
+                if id in prev_selected:
+                    prev_selected.remove(id)
+                else:
+                    prev_selected.append(id)
+        return get_checklist(legende, value=prev_selected), prev_selected
+
+def get_dropdown(legende, value=None):
+    value = [] if value is None else value
+    return html.Div(
+                style={"minHeight": "400px","maxHeight": "400px", "maxWidth": "200px", "minWidth": "200px", "overflow": "auto"},
+                children=[
+                    dcc.Dropdown(
+                        id="namedrop",
+                        options=[{'label': legende[i]['name'], 'value': legende[i]['id']} for i in legende],
+                        value = value,
+                        multi=True
+                    )
+                ],
+        id='dropdown-container'
+    )
+
+def get_checklist(legende, value=None):
+    value = [] if value is None else value
+    return html.Div(
+                style={"maxHeight": "400px", "maxWidth": "200px", "overflow": "auto"},
+                children=[
+                    dcc.Checklist(
+                        id='namelist',
+                        options=[{'label': legende[i]['name'], 'value': legende[i]['id']} for i in legende],
+                        value = value
+                    )
+                ],
+        id='checklist-container'
+    )
 
 def info(datadict):
     text = []
@@ -100,19 +169,43 @@ def info(datadict):
     text = "<br>".join(text)
     return text
 
-def network_graph(nodes, edges, legende, sonder = None):
+def color(nodetype): 
+    # rules for the node color
+    # example, dependend on type
+    if nodetype == "root":
+        return '#000088'
+    if nodetype == "continent":
+        return '#000080'
+    if nodetype == "country":
+        return '#888800'
+    if nodetype == "capital":
+        return '#008888'
+    return '#000000'
 
+def network_graph(nodedict, alledges, legende, search = "", drop = [], sonder = [], check = []):
+    # add nodes from nodedict dependent of check
+    nodes = []
+    for type, node in nodedict.items():
+        if not check or type in check:
+            nodes += node
 
-    # füge die daten Graph hinzu
+    # add nodes to graph
     G = nx.Graph()
     G.add_nodes_from(nodes)
+
+    # add edges from alledges debendent of nodes in graph
+    edges = [edge for edge in alledges if edge[0] in G.nodes and edge[1] in G.nodes]
+
+    # add edges to graph  
     G.add_edges_from(edges)
-    # erstelle positionen und füge sie knoten hinzu
-    pos = nx.layout.kamada_kawai_layout(G) # choose kamada_kawai_layout or spring_layout
+
+    # calculate position with layoutfunction
+    # recommended to choose kamada_kawai_layout or spring_layout
+    pos = nx.layout.kamada_kawai_layout(G)
     for node in G.nodes:
         G.nodes[node]['pos'] = list(pos[node])
 
-    # zeichne graph
+    # drawing graph
     # Create Edges
 
     edge_x = []
@@ -152,39 +245,43 @@ def network_graph(nodes, edges, legende, sonder = None):
     node_x = []
     node_y = []
     node_text = []
+    colors = []
+    if search:
+        search_x = []
+        search_y = []
     for node in G.nodes():
         node = G.nodes[node]
         x, y = node['pos']
         node_x.append(x)
         node_y.append(y)
         node_text.append(info(node))
-        legende[node["_key"]] = {'name': f'{node["name"]}', 'pos': (x, y)}
+        colors.append(color(node['type']))
+        legende[node['_key']] = {'name': node['name'], 'id': node['_id']}
+        if search:
+            if drop:
+                for att in drop:
+                    if att == "_id":
+                        search = search.replace(" ", "_")
+                    if search.lower() in node[att].lower():
+                        search_x.append(x)
+                        search_y.append(y)
+                        break
+            elif search.lower() in str(node).lower():
+                search_x.append(x)
+                search_y.append(y)
 
     node_trace = go.Scatter(
         x=node_x, y=node_y,
         mode='markers',
         hoverinfo='text',
         marker=dict(
-            showscale=True,
-            # colorscale options
-            #'Greys' | 'YlGnBu' | 'Greens' | 'YlOrRd' | 'Bluered' | 'RdBu' |
-            #'Reds' | 'Blues' | 'Picnic' | 'Rainbow' | 'Portland' | 'Jet' |
-            #'Hot' | 'Blackbody' | 'Earth' | 'Electric' | 'Viridis' |
-            colorscale='Greens',
-            reversescale=True,
-            color=[],
+            color=colors,
             size=10,
-            colorbar=dict(
-                thickness=15,
-                title='Node Connections',
-                xanchor='left',
-                titleside='right'
-            ),
-            line_width=2))
+            line_width=1))
             
-    if sonder:
-        sonder_trace = go.Scatter(
-            x=[sonder['pos'][0]], y=[sonder['pos'][1]],
+    if search:
+        search_trace = go.Scatter(
+            x=search_x, y=search_y,
             mode='markers',
             hoverinfo='text',
             marker=dict(
@@ -193,20 +290,34 @@ def network_graph(nodes, edges, legende, sonder = None):
                 line_width=1)
         )
     else:
+        search_trace = go.Scatter(x=[], y=[])
+
+    if sonder:
+        sonder_x = []
+        sonder_y = []
+        for value in sonder:
+            if value in G.nodes().keys():
+                x, y = G.nodes[value]["pos"]
+                sonder_x.append(x)
+                sonder_y.append(y)
+        sonder_trace = go.Scatter(
+            x=sonder_x, y=sonder_y,
+            mode='markers',
+            hoverinfo='text',
+            marker=dict(
+                color='#080',
+                size=15,
+                line_width=1)
+        )
+    else:
         sonder_trace = go.Scatter(x=[], y=[])
-    # Color Node Points
 
-    node_adjacencies = []
-    for node, adjacencies in enumerate(G.adjacency()):
-        node_adjacencies.append(len(adjacencies[1]))
-
-    node_trace.marker.color = node_adjacencies
     node_trace.text = node_text
     middle_trace.text = edge_text
 
     # Create Network Graph
 
-    fig = go.Figure(data=[edge_trace, node_trace, middle_trace, sonder_trace],
+    fig = go.Figure(data=[edge_trace, node_trace, middle_trace, sonder_trace, search_trace],
                 layout=go.Layout(
                     showlegend=False,
                     hovermode='closest',
