5999533a955637c0573f7b0adec806ab008107b6
Tech <pikatech@mail.de>
2020-11-26 13:39:20 +0100

    Update code to work with dictionary

--- scicopia/initusercollection.py
+++ scicopia/initusercollection.py
@@ -1,18 +1,18 @@
 from pyArango.connection import Connection
 from pyArango.theExceptions import CreationError
 
-from config import read_config
+from .config import read_config
 
 config = read_config()
 
 arangoconn = Connection(
-    username=config.username, password=config.password
+    username=config["username"], password=config["password"]
 )  # connection to ArangoDB
-if arangoconn.hasDatabase(config.database):
-    db = arangoconn[config.database]
+if arangoconn.hasDatabase(config["database"]):
+    db = arangoconn[config["database"]]
 else:
-    db = arangoconn.createDatabase(name=config.database)
-if db.hasCollection(config.usercollection):
-    coll = db[config.usercollection]
+    db = arangoconn.createDatabase(name=config["database"])
+if db.hasCollection(config["usercollection"]):
+    coll = db[config["usercollection"]]
 else:
-    coll = db.createCollection(name=config.usercollection)
+    coll = db.createCollection(name=config["usercollection"])
