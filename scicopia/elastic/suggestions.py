637f9ffe709e64d5b7c76e4f55cacea10ae2a5c1
Tech <pikatech@mail.de>
2021-04-12 16:21:37 +0200

    Import suggestions in bulk

--- scicopia/elastic/suggestions.py
+++ scicopia/elastic/suggestions.py
@@ -11,8 +11,11 @@ import sys
 from collections import Counter
 from pathlib import Path
 from pickle import UnpicklingError
+from typing import Any, Dict, Iterator, Iterable
 
 import elasticsearch
+from elasticsearch.helpers import streaming_bulk
+from elasticsearch.exceptions import ConnectionTimeout
 from elasticsearch_dsl import Completion, Document, connections
 from tqdm import tqdm
 
@@ -38,11 +41,35 @@ class Suggestion(Document):
 def store_suggestions(phrases: Counter):
     conn = connections.create_connection(hosts=config["es_hosts"])
     Suggestion.init()
-    for phrase in tqdm(phrases.keys()):
-        doc = Suggestion()
-        doc["keywords_suggest"] = phrase
-        doc.save()
 
+    try:
+        for ok, action in tqdm(streaming_bulk(
+            index=config["suggestions"],
+            client=conn,
+            actions=index(phrases),
+            raise_on_error=False,
+            request_timeout=60,
+            chunk_size=10_000,
+            max_chunk_bytes=10*1024**2,
+        )):
+            if not ok and action["index"]["status"] != 409:
+                logger.warning(action)
+    except ConnectionTimeout as e:
+        logger.warning(
+            "Timeout occurred while processing suggestions"
+        )
+
+
+def index(suggestions: Iterable) -> Iterator[Dict[str, Any]]:
+    for i, suggestion in enumerate(suggestions.items(), 1):
+        doc = {
+            "_op_type": "index",
+            "_index": config["suggestions"],
+            "_id": i
+        }
+        doc["_source"] = {"keywords_suggest": suggestion[0], "weight": suggestion[1]}
+        print(doc)
+        yield doc
 
 if __name__ == "__main__":
     PARSER = argparse.ArgumentParser(
