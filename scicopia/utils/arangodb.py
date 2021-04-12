7e4b93bd87d068b173bfeed8bc7d67c75eb3f051
Tech <pikatech@mail.de>
2021-04-12 16:15:12 +0200

    Select DB and get_docs

--- scicopia/utils/arangodb.py
+++ scicopia/utils/arangodb.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Tue Aug 27 16:18:45 2019
+Created on Wed Apr 7 12:54:23 2021
 
 This module implements commonly used methods like connecting to
 an ArangoDB instance.
@@ -11,9 +11,10 @@ an ArangoDB instance.
 
 @author: tech
 """
-from typing import Dict
+from typing import Collection, Dict
 
 from pyArango.connection import Connection
+from pyArango.database import Database
 
 from scicopia.exceptions import ConfigError, DBError
 
@@ -59,3 +60,77 @@ def connect(config: Dict) -> Connection:
         raise DBError(f"Connection to the ArangoDB server failed.")
     else:
         return conn
+
+def select_db(config: Dict, connection: Connection, create=False) -> Database:
+    """
+    Access a database inside an ArangoDB instance, after connecting
+    to it.
+
+    Parameters
+    ----------
+    config : Dict
+        A configuration file containing the database settings,
+        including a key-value pair "database", holding the
+        name of the ArangoDB database to access.
+    connection : Connection
+        A connection to the ArangoDB instance
+    create : bool, optional
+        Create the database if it doesn't exist, by default False
+    
+    Returns
+    -------
+    Database
+        A reference to the database mentioning the config file under
+        "database", if it exists.
+
+    Raises
+    ------
+    ConfigError
+        If the "database" entry is missing in the config file.
+    DBError
+        If the database to the ArangoDB server failed or
+        the collection cannot be found.
+    """
+    if not "database" in config:
+        raise ConfigError("Setting missing in config file: 'database'.")
+    if connection.hasDatabase(config["database"]):
+        return connection[config["database"]]
+    elif create:
+        return connection.createDatabase(name=config["database"])
+    else:
+        raise DBError(f"Database '{config['database']}' not found.")
+
+
+def get_docs(config: Dict, db: Database) -> Collection:
+    """
+    Access a collection of documents with an ArangoDB database.
+
+    Parameters
+    ----------
+    config : Dict
+        A configuration file containing the database settings,
+        including "documentcollection", which holds the name
+        of the ArangoDB Collection in which the documents are
+        stored.
+    db : Database
+        An ArangoDB database handle
+
+    Returns
+    -------
+    Collection
+        A collection of documents stored in ArangoDB
+
+    Raises
+    ------
+    ConfigError
+        If the "documentcollection" entry is missing in the config file.
+    DBError
+        If the connection to the ArangoDB server failed or
+        the collection cannot be found.
+    """
+    if not "documentcollection" in config:
+        raise ConfigError("Setting missing in config file: 'documentcollection'.")
+    if db.hasCollection(config["documentcollection"]):
+        return db[config["documentcollection"]]
+    else:
+        raise DBError(f"Collection '{config['documentcollection']}' not found.")
