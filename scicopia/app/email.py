9a605a74319c9769ed4b685ce2261c22d31aaeff
RonjaKarmann <ronja.maria.karmann@uni-jena.de>
2021-06-04 15:03:56 +0200

    update flask_mailman to 0.2.3

--- scicopia/app/email.py
+++ scicopia/app/email.py
@@ -1,28 +1,22 @@
 from threading import Thread
 
 from flask import current_app, render_template
-from flask_mailman import Message
+from flask_mailman import EmailMultiAlternatives
 
 from . import mail
 
 
 def send_email(to, subject, template, **kwargs):
     app = current_app._get_current_object()
-    msg = Message(
-        app.config["MAIL_SUBJECT_PREFIX"] + " " + subject,
-        sender=app.config["MAIL_SENDER"],
-        recipients=[to],
+    msg = EmailMultiAlternatives(
+        subject=app.config["MAIL_SUBJECT_PREFIX"] + " " + subject,
+        body = render_template(template + ".txt", **kwargs),
+        from_email=app.config["MAIL_SENDER"],
+        to=[to],
     )
-    msg.body = render_template(template + ".txt", **kwargs)
-    msg.html = render_template(template + ".html", **kwargs)
-    thr = Thread(target=send_async_email, args=[app, msg])
-    thr.start()
-    return thr
-
-
-def send_async_email(app, msg):
-    with app.app_context():
-        mail.send(msg)  # flask_mailman doesn't send in testing mode
-        if app.testing:  # writes only to hard disk, if in testing mode
-            with open("email.txt", "wt", encoding="utf-8") as out:
-                out.write(str(msg))
+    html_content = render_template(template + ".html", **kwargs)
+    msg.attach_alternative(html_content, "text/html")
+    msg.send() # flask_mailman doesn't send in testing mode
+    if app.testing:  # writes only to hard disk, if in testing mode
+        with open("email.txt", "wt", encoding="utf-8") as out:
+            out.write(msg.body)
