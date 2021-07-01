21068b87c8b80b8bfd9778cd4b08e3709b3f834e
RonjaKarmann <ronja.maria.karmann@uni-jena.de>
2021-07-01 21:28:40 +0200

    add docstring

--- scicopia/app/db.py
+++ scicopia/app/db.py
@@ -15,6 +15,19 @@ from .parser.ScicopiaParser import ScicopiaParser
 
 
 def link(texts: List[str]) -> List[str]:
+    """
+    Adds the html a href tag to urls
+
+    Parameters
+    ----------
+    texts : List[str]
+        text that has to be formatted
+
+    Returns
+    -------
+    List[str]
+        formatted text
+    """    
     URL_MATCHER = current_app.config["URL_MATCHER"]
     parts = []
     for text in texts:
@@ -23,6 +36,19 @@ def link(texts: List[str]) -> List[str]:
 
 
 def analyze_input(input: str) -> Dict[str, Dict[str, str]]:
+    """
+    Converts the text to condition usable for Elasticsearch
+
+    Parameters
+    ----------
+    input : str
+        Text with the Search query of a user
+
+    Returns
+    -------
+    Dict[str, Dict[str, str]]
+        Search Conditions for Elasticsearch
+    """    
     input_stream = InputStream(input)
     lexer = ScicopiaLexer(input_stream)
     stream = CommonTokenStream(lexer)
@@ -45,6 +71,23 @@ def analyze_input(input: str) -> Dict[str, Dict[str, str]]:
 
 
 def checkFields(condition, fields):
+    """
+    Corrects the given Fields to search on if possible or removes them
+
+    Parameters
+    ----------
+    condition : Dict[str, Dict[str, str]]
+        Condition of the execute query
+    fields : Dict[str, str]
+        Fields used in the Elasticsearch data
+
+    Returns
+    -------
+    Dict[str, Dict[str, str]]
+        Corrected Condition
+    str
+        tag if field is tags for taglist
+    """    
     for typ, cond in condition.items():  # one pass
         for field in cond:  # one pass
             field = field.lower()
@@ -83,6 +126,9 @@ def checkFields(condition, fields):
 
 
 def newsearch():
+    """
+    Builds the new searchtext for the searchfield
+    """    
     query = []
     for condition in session["condition"]["must"]:
         for _, cond in condition.items():  # one pass
@@ -111,6 +157,41 @@ def newsearch():
 
 
 def execute_query():
+    """
+    Converts given Conditions and executes the search with Elasticsearch
+
+    Parameters 
+    ----------
+    from Session:
+    condition : Dict[str, Dict[str, str]]
+        Conditions of the execute query
+    order : str
+        order to sort results
+    from_hit : int
+        begin of hit range
+    to_hit : int
+        end of hit range
+
+    from config:
+    fields : Dict[str, str]
+        Fields used in the Elasticsearch data
+    search : 
+        Elasticsearch searchclient
+
+    Returns
+    -------
+    g.hits :  List[Dict]
+        List with all hits in the Range
+    session["showfulltext"] : bool
+        fulltext of new search wont be shown
+    session["tags"] : List[Dict]
+        List of 10 most frequent tags from hits with name, number and markedstatus
+    session["time"] : str
+        Time needed to execute the query (convertion of query not included)
+    session["total_hits"] : int
+        number of all hits
+    
+    """    
     conditions = []
     restrictions = []
     fields = current_app.config["FIELDS"]
@@ -197,6 +278,17 @@ def execute_query():
 
 
 def add_search(data):
+    """
+    Adds the given Searchquery to the userprofil in ArangoDB 
+
+    Parameters
+    ----------
+    data : str
+        Searchquery to save
+
+    lastsearch : List[str]
+        List of the 5 last searchquerys of the user
+    """    
     try:
         search = current_app.config["USERCOLLECTION"][session["user"]]["lastsearch"]
     except:
@@ -214,12 +306,42 @@ def add_search(data):
 
 
 def verify_password(user, password):
+    """
+    Checks if the password matchs the password of the user
+
+    Parameters
+    ----------
+    user : str
+        user whos password is to verify
+    password : str
+        password to verify
+
+    Returns
+    -------
+    bool
+        true if password is same as password of user
+    """    
     return check_password_hash(
         current_app.config["USERCOLLECTION"][user]["password_hash"], password
     )
 
 
 def reset_password(token, new_password):
+    """
+    Saves a new password for the user encoded in token
+
+    Parameters
+    ----------
+    token : str
+        encoded data with user
+    new_password : str
+        new password to save
+
+    Returns
+    -------
+    bool
+        success of reset
+    """    
     s = Serializer(current_app.config["SECRET_KEY"])
     try:
         data = s.loads(token.encode("utf-8"))
@@ -238,11 +360,41 @@ def reset_password(token, new_password):
 
 
 def generate_confirmation_token(user, expiration=3600):
+    """
+    Generates Token for user
+
+    Parameters
+    ----------
+    user : str
+        user to encode
+    expiration : int, optional
+        time to use the toke, by default 3600
+
+    Returns
+    -------
+    str
+        token with encoded user
+    """    
     s = Serializer(current_app.config["SECRET_KEY"], expiration)
     return s.dumps({"confirm": user}).decode("utf-8")
 
 
 def confirm_u(user, token):
+    """
+    Saves in ArangoDB if user successfully used his confirmation link
+
+    Parameters
+    ----------
+    user : str
+        to confirm
+    token : str
+        encoded user
+
+    Returns
+    -------
+    bool
+        success of confirm
+    """    
     s = Serializer(current_app.config["SECRET_KEY"])
     try:
         data = s.loads(token.encode("utf-8"))
